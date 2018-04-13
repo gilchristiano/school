8936704fefd50ce9f67cbfbbaa8afdfb5bb36835
Gil Christiano Guedes dos Santos <gilchristiano@yahoo.com.br>
2018-04-13 17:12:54 -0300

    Plugin QRCode
    Para jQuery e Javascript

--- actions/ViewStudent.php
+++ actions/ViewStudent.php
@@ -7,24 +7,26 @@ class MyDB extends SQLite3
 {
     function __construct()
     {
-        $this->open('../escolar.db');
+        $this->open('../school.db');
     }
 }
-
+ 
 $db = new MyDB();
 // $db->exec("INSERT INTO employee(codigo,name,surname,CPF,RG,blood,filiacao,dateOfBirth,office) VALUES('123','Robson','Monteiro','1232424','242424','A+','Jurema','24/04/2024','Vigilante')");
-
+	include("../view/ViewPerson.php");
+	include("../view/header.php"); // cabeçalho
+	/*
 $result = $db->query('SELECT * FROM student where codigo = 1234 ');
-	include("../view/ViewStudent.php");
 while ($outdata = $result->fetchArray()){
 	$student = new ViewStudent($outdata);
 	$student->ViewStudent(); // Saída na tela
 	}
-	
-$result = $db->query('SELECT * FROM employee where codigo = 123 ');
+	*/
+$result = $db->query('SELECT * FROM employee ORDER BY name');
 while ($outdata = $result->fetchArray()){
 	$employee = new ViewEmployee($outdata);
 	$employee->ViewEmployee(); // Saída na tela
 	}
+	include("../view/footnote.php"); // rodapé
 
 ?>
---
+++ view/header.php
@@ -0,0 +1,15 @@
+<?php
+echo "<!DOCTYPE html>";
+echo "<html xmlns='http://www.w3.org/1999/xhtml'>";
+echo "    <head>";
+echo "    <head>";
+echo "		<title>Acesso ao Instituto</title>";
+echo "		<meta name='viewport' content='width=device-width, initial-scale=1'>";
+echo "		<meta charset='UTF-8'/>";
+echo "		<link rel='stylesheet' href='http://code.jquery.com/mobile/1.1.1/jquery.mobile-1.1.1.min.css' />";
+echo "		<script src='http://code.jquery.com/jquery-1.7.1.min.js'></script>";
+echo "		<script src='http://code.jquery.com/mobile/1.1.1/jquery.mobile-1.1.1.min.js'></script>";
+echo "    </head>";
+echo "    <body>";
+echo "       <div data-role='page' id='page1'>";
+?>
